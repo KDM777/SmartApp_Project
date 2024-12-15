3a0a1a9c136e6ba3e2d15ba74bc9e2844566f8af
KDM777 <daemingim5@gmail.com>
2024-12-16 00:28:17 +0900

    🚧 IIAList 완성

--- app/src/main/java/kr/ac/kumoh/ce/s20200124/project/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20200124/project/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -15,7 +19,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import kr.ac.kumoh.ce.s20200124.project.ui.theme.ProjectTheme
 
@@ -46,10 +55,35 @@ fun MainScreen(viewModel: IIAViewModel= androidx.lifecycle.viewmodel.compose.vie
 @Composable
 fun IIAList(list: List<IIA>, modifier: Modifier) {
     LazyColumn (
-        modifier
+        modifier,
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding= PaddingValues(horizontal=8.dp)
     ){
         items(list){iia->
-            Text(iia.toString())
+            IIAItem(iia)
         }
     }
+}
+
+@Composable
+fun IIAItem(iia:IIA){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color(0xffffffcc))
+            .padding(16.dp)
+    ) {
+        TextName(iia.name)
+        TextInterest(iia.interest)
+    }
+}
+
+@Composable
+fun TextName(name:String){
+    Text(name, fontSize = 30.sp)
+}
+
+@Composable
+fun TextInterest(interest:String){
+    Text(interest, fontSize = 20.sp)
 }
